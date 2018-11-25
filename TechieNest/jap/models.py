f95b6ac2fd9c66514e0b021333101f6e6b83a75b
amoghparab247 <mumbai.amoghparab@gmail.com>
2018-11-25 13:42:40 +0530

    registration form created

--- TechieNest/jap/models.py
+++ TechieNest/jap/models.py
@@ -11,9 +11,43 @@ class TrainingRegistrationForm(models.Model):
 	ss = '-- select state --'
 	an = 'Andaman & Nicobar Island'
 	ap = 'Arunachal Pradesh'
-	#do the same as above for all the choices listed below
+	anp = 'Andra Pradesh'
+	a = 'Assam'
+	b = 'Bihar'
+	cd = 'Chandigarh'
+	ct = 'Chattisgarh'
+	d = 'Delhi'
+	dn = 'Dadra & Nagar Haveli'
+	dd = 'Daman & Diu'
+	g = 'Goa'
+	gj = 'Gujarat'
+	h = 'Haryana'
+	hp = 'Himachal Pradesh'
+	jk = 'Jammu & Kashmir'
+	j = 'Jharkhand'
+	k = 'Karnataka'
+	kr = 'Kerela'
+	l = 'Lakshadweep'
+	mp = 'Madhya Pradesh'
+	m = 'Maharashtra'
+	ma = 'Manipur'
+	me = 'Meghalaya'
+	mi = 'Mizoram'
+	n = 'Nagaland'
+	p = 'Pondicherry'
+	o = 'Odisha'
+	p = 'Punjab'
+	r = 'Rajasthan'
+	s = 'Sikkim'
+	tn = 'Tamil Nadu'
+	tr = 'Tripura'
+	te = 'Telangana'
+	up = 'Uttar Pradesh'
+	uk = 'Uttarakhand'
+	uc = 'Uttaranchal'
+	wb = 'West Bengal'
 	state_choices = (
-			(ss, '-- select state --'),(an, 'Andaman & Nicobar Island'),(ap, 'Arunachal Pradesh'),(anp, 'Andra Pradesh'),(a, 'Assam'),('Bihar'),
+			(ss, '-- select state --'),(an, 'Andaman & Nicobar Island'),(ap, 'Arunachal Pradesh'),(anp, 'Andra Pradesh'),(a, 'Assam'),(b ,'Bihar'),
 			(cd, 'Chandigarh'),(ct, 'Chattisgarh'),(d, 'Delhi'),(dn, 'Dadra & Nagar Haveli'),(dd, 'Daman & Diu'),(g, 'Goa'),
 			(gj, 'Gujarat'),(h, 'Haryana'),(hp, 'Himachal Pradesh'),(jk, 'Jammu & Kashmir'),(j, 'Jharkhand'),(k, 'Karnataka'),
 			(kr, 'Kerela'),(l, 'Lakshadweep'),(mp, 'Madhya Pradesh'),(m, 'Maharashtra'),(ma, 'Manipur'),(me, 'Meghalaya'),
@@ -24,19 +58,41 @@ class TrainingRegistrationForm(models.Model):
 	state = models.CharField(max_length = 100, choices = state_choices, default = ss)
 	college = models.CharField(max_length = 100, blank = False)
 	branch = models.CharField(max_length = 100, blank = False)
+	scs = '-- current semester --'
+	one = '1'
+	two = '2'
+	three = '3'
+	four = '4'
+	five = '5'
+	six = '6'
+	seven = '7'
+	eight = '8'
+	passout = 'Passout'
 	current_semester_choices = (
-			('-- current semester --'),('1'),('2'),('3'),('4'),('5'),
-			('6'),('7'),('8'),('Passout'),
+			(scs, '-- current semester --'),(one, '1'),(two, '2'),(three, '3'),(four, '4'),(five, '5'),
+			(six, '6'),(seven, '7'),(eight, '8'),(passout, 'Passout'),
 		)
 	current_semester = models.CharField(max_length = 50, choices = current_semester_choices, default = '-- current semester --')
+	srf = '-- select registration for --'
+	winter = 'Winter industrial internship/training program'
+	summer =  'Summer industrial internship/training program'
+	regular = 'Regular training program'
+	week = 'Weekend training program'
+	jap = 'Job assurance program'
 	registration_for_choices = (
-			('-- select registration for --'),('Summer industrial internship/training program'),('Winter industrial internship/training program'),
-			('Regular training program'),('Weekend training program'),('Job assurance program'),
+			(srf, '-- select registration for --'),(summer, 'Summer industrial internship/training program'),(winter, 'Winter industrial internship/training program'),
+			(regular, 'Regular training program'),(week, 'Weekend training program'),(jap, 'Job assurance program'),
 		)
 	registration_for = models.CharField(max_length =100, choices = registration_for_choices, default = '-- select registration for --')
+	spe = '-- select --'
+	sitp = 'SITP'
+	witp = 'WITP'
+	wor = 'Workshop'
+	inte = 'Internship'
+	oth = 'Others'
 	previous_exp_choices = (
-			('-- select --'),('SITP'),('WITP'),('Workshop'),('Internship'),('Others'),
+			(spe, '-- select --'),(sitp, 'SITP'),(witp, 'WITP'),(wor, 'Workshop'),(inte, 'Internship'),(oth, 'Others'),
 		)
-	previous_exp = models.CharField(max_length = 200, choices = previous_exp_choices, default = '-- select --')
+	previous_exp = models.CharField(max_length = 200, choices = previous_exp_choices, default = spe)
 	how_did_you_know = models.CharField(max_length = 200)
 	techienest_contactperson = models.CharField(max_length = 10)
